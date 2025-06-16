20ec3e829423ee8fdf6f15665eab5150c98f6bf8
ashjackson82 <ashjackson82@gmail.com>
2025-06-16 11:16:18 -0400

    I: EXPLOSIONSgit add .!

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
 from circleshape import CircleShape
+from pygame.locals import *
+from vfx import Explosion
 
 def main():
     # Game initialization
@@ -30,6 +32,8 @@ def main():
     field = AsteroidField()
     shots = pygame.sprite.Group()
     Shot.containers = (shots, updatable, drawable)
+    explosion_group = pygame.sprite.Group()
+
 
     # Game loop
     while True:
@@ -44,11 +48,15 @@ def main():
             for shot in shots:
                 if asteroid.collision(shot) == True:
                     pygame.sprite.Sprite.kill(shot)
+                    exp = Explosion(asteroid.position.x,asteroid.position.y)
+                    explosion_group.add(exp)
                     asteroid.split()
 
-        screen.fill((0,0,0))
+        screen.fill((5,5,5))
         for obj in drawable:
             obj.draw(screen)
+        explosion_group.draw(screen)
+        explosion_group.update()   
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
